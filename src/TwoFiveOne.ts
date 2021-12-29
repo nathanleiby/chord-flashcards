6068681898e18f904b42fe62e4e5f3e9b1d10648
Nathan Leiby <nathanleiby@gmail.com>
2021-12-29 12:03:13 -0800

    remove unused var

--- src/TwoFiveOne.ts
+++ src/TwoFiveOne.ts
@@ -1,7 +1,5 @@
 import { Chord, Interval, Note } from "@tonaljs/tonal";
 
-const qualities = ["major", "minor", "diminished"];
-
 const intervals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((x) =>
   Interval.fromSemitones(x)
 );
