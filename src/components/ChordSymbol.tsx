6712bf017889b22998bbf4d25ce97feb594b154f
Nathan Leiby <nathanleiby@gmail.com>
2022-11-18 16:06:30 -0600

    try Noto font for music
    https://fonts.google.com/noto/specimen/Noto+Music/about?query=music

--- src/components/ChordSymbol.tsx
+++ src/components/ChordSymbol.tsx
@@ -7,7 +7,7 @@ export interface ChordSymbolProps {
   chordSymbol: string;
 }
 
-const FONT_SIZE = 20;
+const FONT_SIZE = 32;
 export default function ChordSymbol(props: ChordSymbolProps) {
   const { chordSymbol } = props;
   const [id] = useState(_.uniqueId("chord-symbol-"));
@@ -18,27 +18,11 @@ export default function ChordSymbol(props: ChordSymbolProps) {
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.font = `${FONT_SIZE}px Petaluma Script`;
+    ctx.font = `${FONT_SIZE}px Noto`;
 
     const formatter = new BbFormat(ctx);
 
-    // // Modify the formatter's config
-    // const options = new BbChordSymbolOptions();
-    // options.parentheses.type = "[]";
-    // options.useMinusSignForMinorChords = false;
-    // options.descriptorVerticalOffset = 0;
-    // options.separator.angle = (Math.PI * 20) / 360;
-
-    // formatter.chordSymbolOptions = options;
-
-    // // tweak the chord's pre-layout renderer's config
-    // formatter.rendererOptions = {
-    //   useShortNamings: false,
-    //   harmonizeAccidentals: true,
-    //   transposeValue: 2, // transpose a whole tone above
-    // };
-
-    formatter.fillChordSymbol(chordSymbol, 0, FONT_SIZE / 2);
+    formatter.fillChordSymbol(chordSymbol, 0, Math.ceil(FONT_SIZE / 2));
   }, [chordSymbol]);
 
   return (
@@ -46,7 +30,7 @@ export default function ChordSymbol(props: ChordSymbolProps) {
       id={id}
       ref={ref}
       width={100}
-      height={FONT_SIZE * 2}
+      height={FONT_SIZE + Math.ceil(FONT_SIZE / 2)}
       className="chord-symbol"
     />
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import "./fonts/noto.ttf";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
